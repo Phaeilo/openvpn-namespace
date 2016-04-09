f90cce63598ba3f0b06f54a8f0f444df710636ce
Philip Huppert <philip@zeilen-sprung.de>
2016-04-09 23:57:09 +0200

    refactored updown.py

--- updown.py
+++ updown.py
@@ -1,110 +1,152 @@
 #!/usr/bin/env python3
 
 import os
-import subprocess as sp
+import sys
+import shlex
+import subprocess
 
-#import sys
-#for key in os.environ.keys():
-#    print("%30s %s \n" % (key,os.environ[key]))
-#sys.exit(1)
+
+NAMESPACE = "ns1"
+DEBUG_ENV = False
+
+
+if DEBUG_ENV:
+    # print all environment variables and exit
+    # useful for debugging what configuration data is made available by OpenVPN
+    for key in os.environ.keys():
+        sys.stderr.write("%30s %s \n" % (key, os.environ[key]))
+    sys.exit(1)
+
 
 def call(cmd, args):
-    cmd = cmd.split(" ")
+    """
+    Run the command specified in cmd. Arguments consisting of a single ? are
+    replaced with the corresponding value from args. This function returns if
+    the command was executed successfully. If the command caused an error, an
+    exception is thrown.
+    """
+
+    # split command into its arguments
+    assert cmd is not None
+    cmd = shlex.split(cmd)
+
+    # replace placeholders with contents from args
     j = 0
     for i in range(len(cmd)):
         if cmd[i] == "?":
             cmd[i] = str(args[j])
             j += 1
 
-    sp.check_call(cmd)
+    # run command
+    subprocess.check_call(cmd)
 
 
 def mask_to_cidr(mask):
+    """
+    Determine the CIDR suffix for a given dotted decimal IPv4 netmask.
+    """
+    # convert netmask to 32 binary digits
     tmp = "".join([format(int(x), "08b") for x in mask.split(".")])
-    return len(tmp.partition("0")[0])
+    # count leading ones
+    return len(tmp) - len(tmp.lstrip("1"))
 
 
+# VPN connection setUP or tearDOWN
 script_type = os.getenv("script_type")
 assert script_type in ("up", "down")
 
+# NIC properties
 device = os.getenv("dev")
 tun_mtu = int(os.getenv("tun_mtu"))
 
+# IPv4 configuration
 v4_addr = os.getenv("ifconfig_local")
 v4_mask = os.getenv("ifconfig_netmask")
 v4_gateway = os.getenv("route_vpn_gateway")
 
-dns = []
+# DNS configuration
+dns_servers = []
 domain = None
-for i in range(1,99):
+i = 1
+while True:
     o = os.getenv("foreign_option_%d" % i)
+    i += 1
+
     if o is None:
         break
+
     elif o.startswith("dhcp-option DNS"):
         dns_ip = o.partition(" DNS ")[2]
-        dns.append(dns_ip)
+        dns_servers.append(dns_ip)
+
     elif o.startswith("dhcp-option DOMAIN"):
         domain = o.partition(" DOMAIN ")[2]
 
-
-namespace = "ns1"
-namespace_dir = os.path.join("/etc/netns", namespace)
-dns_config = os.path.join(namespace_dir, "resolv.conf")
-
+# setup
 if script_type == "up":
     # create namespace
     try:
-        call("ip netns add ?", (namespace,))
+        call("ip netns add ?", (NAMESPACE,))
     except:
         # ignore if namespace exists
         pass
 
+    def nsexec(cmd, args=()):
+        cmd = "ip netns exec ? " + cmd
+        args = (NAMESPACE,) + args
+        call(cmd, args)
+
     # configure firewall in namespace
-    call("ip netns exec ? iptables -F", (namespace,))
-    call("ip netns exec ? iptables -I INPUT -j DROP", (namespace,))
-    call("ip netns exec ? iptables -I FORWARD -j DROP", (namespace,))
-    call("ip netns exec ? iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT", (namespace,))
-    call("ip netns exec ? iptables -I INPUT -i lo -j ACCEPT", (namespace,))
-    call("ip netns exec ? ip6tables -F", (namespace,))
-    call("ip netns exec ? ip6tables -I INPUT -j DROP", (namespace,))
-    call("ip netns exec ? ip6tables -I INPUT -i lo -j ACCEPT", (namespace,))
-    call("ip netns exec ? ip6tables -I FORWARD -j DROP", (namespace,))
-    call("ip netns exec ? ip6tables -I OUTPUT -j DROP", (namespace,))
-    call("ip netns exec ? ip6tables -I OUTPUT -o lo -j ACCEPT", (namespace,))
+    nsexec("iptables -F")
+    nsexec("iptables -I INPUT -j DROP")
+    nsexec("iptables -I FORWARD -j DROP")
+    nsexec("iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
+    nsexec("iptables -I INPUT -i lo -j ACCEPT")
+    nsexec("ip6tables -F")
+    nsexec("ip6tables -I INPUT -j DROP")
+    nsexec("ip6tables -I INPUT -i lo -j ACCEPT")
+    nsexec("ip6tables -I FORWARD -j DROP")
+    nsexec("ip6tables -I OUTPUT -j DROP")
+    nsexec("ip6tables -I OUTPUT -o lo -j ACCEPT")
 
     # move device to namespace
-    call("ip link set ? netns ?", (device, namespace))
+    call("ip link set ? netns ?", (device, NAMESPACE))
 
     # enable loopback device in namespace
-    call("ip netns exec ? ip link set lo up", (namespace,))
+    nsexec("ip link set lo up")
 
     # enable device
-    call("ip netns exec ? ip link set ? up", (namespace, device))
+    nsexec("ip link set ? up", (device,))
 
     # set mtu
-    call("ip netns exec ? ip link set dev ? mtu ?", (namespace, device, tun_mtu))
+    nsexec("ip link set dev ? mtu ?", (device, tun_mtu))
 
     # configure v4 address
     address = "%s/%d" % (v4_addr, mask_to_cidr(v4_mask))
-    call("ip netns exec ? ip addr change ? dev ?", (namespace, address, device))
+    nsexec("ip addr change ? dev ?", (address, device))
 
     # add default route
-    call("ip netns exec ? ip route add default via ?", (namespace, v4_gateway))
+    nsexec("ip route add default via ?", (v4_gateway,))
 
-    # configure dns
+    # configure DNS
+    namespace_dir = os.path.join("/etc/netns", NAMESPACE)
     if not os.path.exists(namespace_dir):
         os.makedirs(namespace_dir)
+
+    dns_config = os.path.join(namespace_dir, "resolv.conf")
     with open(dns_config, "w") as fh:
         if domain is not None:
             fh.write("domain " + domain + "\n")
-        for d in dns:
-            fh.write("nameserver " + d + "\n")
+        for dns_server in dns_servers:
+            fh.write("nameserver " + dns_server + "\n")
 
+# teardown
 if script_type == "down":
     # delete namespace
-    call("ip netns delete ?", (namespace,))
+    call("ip netns delete ?", (NAMESPACE,))
 
-    # unconfigure dns
+    # unconfigure DNS
+    namespace_dir = os.path.join("/etc/netns", NAMESPACE)
+    dns_config = os.path.join(namespace_dir, "resolv.conf")
     os.remove(dns_config)
     os.rmdir(namespace_dir)
-
